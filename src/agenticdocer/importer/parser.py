bba28236a445c6609332dcc873a0349f24ad2f9d
amoslee2026 <286830877+amoslee2026@users.noreply.github.com>
2026-09-16 19:35:01 +0800

    [omp-checkpoint] 2026-09-16 19:35:01 (UTC+8)

--- src/agenticdocer/importer/parser.py
+++ src/agenticdocer/importer/parser.py
@@ -1010,7 +1010,8 @@ def parse_text(
             )
             proposals.append(
                 Proposal(
-                    proposal_id=proposal_id,
+                    atom_type=item.atom_type,
+                    format=item.format,  # type: ignore[arg-type]
                     rule_id=item.block.rule_id,
                     confident=item.block.confident,
                     source_lines=item.source_lines,
