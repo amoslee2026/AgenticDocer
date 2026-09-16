3b451e70d5222fd55bc9acb855aeaadb1ed68976
amoslee2026 <286830877+amoslee2026@users.noreply.github.com>
2026-09-16 19:29:55 +0800

    [omp-checkpoint] 2026-09-16 19:29:55 (UTC+8)

--- src/agenticdocer/importer/rules.py
+++ src/agenticdocer/importer/rules.py
@@ -315,7 +315,20 @@ _TOC_NUMBERED_RE = re.compile(
     r"Chapter\s+\d+|Appendix\s+[A-Z]|Annex\s+[A-Z]|Part\s+[A-Z])\b.*?[\s.]\d{1,4}[ \t]*$"
 )
 _GLOSSARY_MARKER_RE = re.compile(
-
+    r"^(?:(?:part|appendix|annex|chapter)[ \t]+[A-Z0-9]+(?:[ \t]+[A-Z0-9]+)?[ \t]+)?"
+    r"(?:glossary|terms and acronyms|terms and definitions|terms|definitions|"
+    r"abbreviations|acronyms|definitions and abbreviations|list of abbreviations|"
+    r"terminology)\b",
+    re.IGNORECASE,
+)
+_GLOSSARY_EXIT_RE = re.compile(
+    r"^(?:contents|table of contents|list of tables|list of figures|figures|tables|preface|index|"
+    r"revision history|change history|introduction|notice|disclaimer|legal|about|using this|"
+    r"intended audience|feedback|objective|overview|standard improvement form|acknowledge?ments|"
+    r"copyright|proprietary)\b",
+    re.IGNORECASE,
+)
+_CROSS_REF_RE = re.compile(
     r"^(?:See|Refer to|see|refer to)\b[^.!?]{0,160}?\b"
     r"(?:Section|Chapter|Clause|Table|Figure|Annex|Appendix|Part|sub-section|subsection|"
     r"specification|document)\b",
