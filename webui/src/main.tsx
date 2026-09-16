89487892acda7a80949deb8afdd1867b4ed7e7fe
amoslee2026 <286830877+amoslee2026@users.noreply.github.com>
2026-09-16 20:50:56 +0800

    [omp-checkpoint] 2026-09-16 20:50:56 (UTC+8)

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import "./styles.css";
+import { App } from "./App";
+import { AuthProvider } from "./auth/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
